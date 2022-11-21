58c4799d10398b3594b87377f8d88e3f0d1ff118
SofiiaDe <sofiia.prydatko@gmail.com>
2022-11-21 21:24:47 +0200

    Initial SpringBoot project

---
+++ src/main/java/com/epam/rd/EmotionalHelpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.epam.rd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionalHelpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionalHelpBackendApplication.class, args);
+	}
+
+}
