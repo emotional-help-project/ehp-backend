aca7e225fe676a4a9298f511945ee8c6f42402a0
Javqochdiyev Otabek <djavistdev@mail.ru>
2022-11-22 11:32:52 +0500

    Service and repository for user

---
+++ src/main/java/com/epam/rd/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.epam.rd.repository;
+
+import com.epam.rd.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@EnableJpaRepositories
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+    Optional<User> findUserById(Long id);
+}
