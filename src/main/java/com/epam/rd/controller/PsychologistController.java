9d3afbe43dee1a84dc1531ed5668a3ebf4dc91f8
Javqochdiyev Otabek <djavistdev@mail.ru>
2022-12-13 03:48:10 +0500

    Psycholcontroller changed back

--- src/main/java/com/epam/rd/controller/PsychologistController.java
+++ src/main/java/com/epam/rd/controller/PsychologistController.java
@@ -5,9 +5,11 @@ import com.epam.rd.payload.request.SearchRequest;
 import com.epam.rd.service.PsychologistService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 
 @RestController
@@ -33,9 +35,27 @@ public class PsychologistController {
         return ResponseEntity.ok(psychologistService.getAppointmentCalendarByPsychologist(psychologistId, currentDateTime));
     }
 
+    @PostMapping
+    public ResponseEntity<?> createPsychologist(@Valid @RequestBody PsychologistDto psychologistDto) {
+        return ResponseEntity.ok(psychologistService.createPsychologist(psychologistDto));
+
+    }
+
+    @PutMapping
+    public ResponseEntity<PsychologistDto> updatePsychologist(@Valid @RequestBody PsychologistDto psychologistDto) {
+        return ResponseEntity.ok(psychologistService.updatePsychologist(psychologistDto));
+    }
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<PsychologistDto> getPsychologistById(@PathVariable Long id) {
         return ResponseEntity.ok(psychologistService.getPsychologistById(id));
+    }
+
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePsychologistById(@PathVariable Long id) {
+        psychologistService.deletePsychologist(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/page")
@@ -48,6 +68,7 @@ public class PsychologistController {
     public Page<PsychologistDto> search(@RequestBody SearchRequest request) {
         return psychologistService.searchPsychologist(request);
     }
+
 
 
 }
