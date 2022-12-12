2b024fa1b3d655a9e317c0e7c12436468c9d4779
SofiiaDe <sofiia.prydatko@gmail.com>
2022-12-12 12:08:28 +0200

    Fix validation of update user's profile

--- src/main/java/com/epam/rd/payload/request/UpdateUserProfileRequest.java
+++ src/main/java/com/epam/rd/payload/request/UpdateUserProfileRequest.java
@@ -1,23 +1,33 @@
 package com.epam.rd.payload.request;
 
-import com.epam.rd.annotations.PasswordMatches;
 import com.epam.rd.annotations.ValidEmail;
+import com.epam.rd.annotations.ValidPassword;
 import com.epam.rd.model.enumerations.Gender;
 import lombok.Data;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 
 @Data
-@PasswordMatches
-@ValidEmail
 public class UpdateUserProfileRequest {
 
     private Long id;
+
+    @Size(min = 2, max = 20, message = "First Name should be between 2 and 20 characters")
     private String firstName;
+
+    @Size(min = 2, max = 20, message = "Last Name should be between 2 and 20 characters")
     private String lastName;
+
+    @ValidEmail(message = "Email must be in the correct format")
     private String email;
-    @Size(min = 8)
+
+    @ValidPassword(message = "Password must contain at least one digit, at least one lowercase and one uppercase Latin characters as well as at least one special character like ! @ # & ( ) etc.")
+    @Size(min = 8, message = "Password size must be not less than 8 characters.")
     private String newPassword;
+
     private Gender gender;
+
+    @Min(value = 5, message = "Age must be greater than or equal to 5")
     private int age;
 }
