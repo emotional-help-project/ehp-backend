30f50ed7ed76221cfe05b318ada4f8ddf9178325
SofiiaDe <sofiia.prydatko@gmail.com>
2022-12-16 00:11:25 +0200

    Add new IP for frontend

--- src/main/java/com/epam/rd/security/SecurityConfig.java
+++ src/main/java/com/epam/rd/security/SecurityConfig.java
@@ -195,6 +195,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         config.addAllowedOrigin("http://5.58.12.93:8080");
         config.addAllowedOrigin("http://localhost:4200");
         config.addAllowedOrigin("http://54.187.13.27:8080");
+        config.addAllowedOrigin("http://139.59.170.104:9090");
 
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
