3db9c3d24238ad1ba65542f25ac8a348605a4df7
SofiiaDe <sofiia.prydatko@gmail.com>
2022-12-16 04:20:12 +0200

    Fix getting advice for user passing test

--- src/main/java/com/epam/rd/repository/AdviceRepository.java
+++ src/main/java/com/epam/rd/repository/AdviceRepository.java
@@ -1,6 +1,7 @@
 package com.epam.rd.repository;
 
 import com.epam.rd.model.entity.Advice;
+import com.epam.rd.model.entity.Test;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,7 @@ import java.util.Optional;
 @Repository
 public interface AdviceRepository extends BaseRepository<Advice> {
 
-    @Query("SELECT a FROM Advice a WHERE a.scoreFrom <= :userScore and a.scoreTo >= :userScore")
-    Optional<Advice> findAdviceByUserScore(@Param("userScore") Long userScore);
+    @Query("SELECT a FROM Advice a WHERE a.test = test AND a.scoreFrom <= :userScore and a.scoreTo >= :userScore")
+    Optional<Advice> findAdviceByUserScoreAndTestId(@Param("userScore") Long userScore,
+                                                    @Param("test") Test test);
 }
